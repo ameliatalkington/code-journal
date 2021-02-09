3a4af07318a6d6e00050c1c975f6444d05b91c53
ameliatalkington <amrtalk@stanford.edu>
2021-02-08 17:06:47 -0700

    issue 1 - task #5

--- js/main.js
+++ js/main.js
@@ -1,2 +1,7 @@
 /* global data */
 /* exported data */
+var $photoURL = document.querySelector('#image-url');
+var $displayedImage = document.querySelector('.displayed-image');
+$photoURL.addEventListener('input', function (event) {
+  $displayedImage.setAttribute('src', event.target.value);
+});
