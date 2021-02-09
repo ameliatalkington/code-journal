24271be23f5771830ee7e30afe6ea4bcfb67a97e
ameliatalkington <amrtalk@stanford.edu>
2021-02-09 16:45:16 -0700

    Added anchor to entries to show entry form - ensured new journal entry creates a new DOM tree

--- js/main.js
+++ js/main.js
@@ -5,6 +5,9 @@ var $displayedImage = document.querySelector('.displayed-image');
 var $form = document.querySelector('#form');
 var $title = document.querySelector('#title');
 var $notes = document.querySelector('#notes');
+var $entriesButton = document.querySelector('.entries-button');
+var $newButton = document.querySelector('.new-button');
+var $containerElements = document.querySelectorAll('.container');
 
 $photoURL.addEventListener('input', function (event) {
   $displayedImage.setAttribute('src', event.target.value);
@@ -21,4 +24,14 @@ $form.addEventListener('submit', function (event) {
   data.entries.unshift(newObject);
   data.nextEntryId++;
   $form.reset();
+});
+
+$newButton.addEventListener('click', function (event) {
+  $containerElements[0].className = 'container';
+  $containerElements[1].className = 'container hidden';
+});
+
+$entriesButton.addEventListener('click', function (event) {
+  $containerElements[0].className = 'container hidden';
+  $containerElements[1].className = 'container';
 });
