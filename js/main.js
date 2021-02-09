e00fb5a8938d5a7c33125c1e27612002e5827a3a
ameliatalkington <amrtalk@stanford.edu>
2021-02-09 16:06:41 -0700

    When DOMContentLoaded event is fired a DOM tree is created for every journal entry

--- js/main.js
+++ js/main.js
@@ -5,7 +5,6 @@ var $displayedImage = document.querySelector('.displayed-image');
 var $form = document.querySelector('#form');
 var $title = document.querySelector('#title');
 var $notes = document.querySelector('#notes');
-var $entries = document.querySelector('.container');
 
 $photoURL.addEventListener('input', function (event) {
   $displayedImage.setAttribute('src', event.target.value);
@@ -23,18 +22,3 @@ $form.addEventListener('submit', function (event) {
   data.nextEntryId++;
   $form.reset();
 });
-
-function newEntry(entry) {
-  var $ul = document.createElement('ul');
-  var $divRow = $ul.appendChild(document.createElement('div'));
-  $divRow.setAttribute('class', 'row');
-  var $divColHalf1 = $divRow.appendChild(document.createElement('div'));
-  $divColHalf1.setAttribute('class', 'column-half');
-  var $divColHalf2 = $divRow.appendChild(document.createElement('div'));
-  $divColHalf2.setAttribute('class', 'column-half');
-  var $imgTag = $divColHalf1.appendChild(document.createElement('img'));
-  var $h3 = $divColHalf2.appendChild(document.createElement('h3'));
-  var $p = $divColHalf2.appendChild(document.createElement('p'));
-
-  return($ul);
-}
