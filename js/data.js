9c1efe4ae3e82cdc3a63f758043b10933e1a00c4
ameliatalkington <amrtalk@stanford.edu>
2021-02-09 18:19:43 -0700

    Moved entires function and content loaded to main.js

--- js/data.js
+++ js/data.js
@@ -1,6 +1,5 @@
 /* exported data */
 var previousDataJSON = localStorage.getItem('code-journal-local-storage');
-var $entries = document.querySelector('.row.entries');
 
 var data = {
   view: 'entry-form',
@@ -17,28 +16,3 @@ window.addEventListener('beforeunload', function (event) {
   var dataJSON = JSON.stringify(data);
   localStorage.setItem('code-journal-local-storage', dataJSON);
 });
-
-window.addEventListener('DOMContentLoaded', function (event) {
-  for (var i = 0; i < data.entries.length; i++) {
-    $entries.appendChild(newEntry(data.entries[i]));
-  }
-});
-
-function newEntry(entry) {
-  var $ul = document.createElement('ul');
-  var $divRow = $ul.appendChild(document.createElement('div'));
-  $divRow.setAttribute('class', 'row');
-  var $divColHalf1 = $divRow.appendChild(document.createElement('div'));
-  $divColHalf1.setAttribute('class', 'column-half');
-  var $divColHalf2 = $divRow.appendChild(document.createElement('div'));
-  $divColHalf2.setAttribute('class', 'column-half');
-  var $imgTag = $divColHalf1.appendChild(document.createElement('img'));
-  var $h3 = $divColHalf2.appendChild(document.createElement('h3'));
-  var $p = $divColHalf2.appendChild(document.createElement('p'));
-
-  $imgTag.setAttribute('src', entry.imageUrl);
-  $h3.textContent = entry.title;
-  $p.textContent = entry.notes;
-
-  return ($ul);
-}
--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ var $notes = document.querySelector('#notes');
 var $entriesButton = document.querySelector('.entries-button');
 var $newButton = document.querySelector('.new-button');
 var $containerElements = document.querySelectorAll('.container');
+var $entries = document.querySelector('.row.entries');
 
 $photoURL.addEventListener('input', function (event) {
   $displayedImage.setAttribute('src', event.target.value);
@@ -35,3 +36,28 @@ $entriesButton.addEventListener('click', function (event) {
   $containerElements[0].className = 'container hidden';
   $containerElements[1].className = 'container';
 });
+
+window.addEventListener('DOMContentLoaded', function (event) {
+  for (var i = 0; i < data.entries.length; i++) {
+    $entries.appendChild(newEntry(data.entries[i]));
+  }
+});
+
+function newEntry(entry) {
+  var $ul = document.createElement('ul');
+  var $divRow = $ul.appendChild(document.createElement('div'));
+  $divRow.setAttribute('class', 'row');
+  var $divColHalf1 = $divRow.appendChild(document.createElement('div'));
+  $divColHalf1.setAttribute('class', 'column-half');
+  var $divColHalf2 = $divRow.appendChild(document.createElement('div'));
+  $divColHalf2.setAttribute('class', 'column-half');
+  var $imgTag = $divColHalf1.appendChild(document.createElement('img'));
+  var $h3 = $divColHalf2.appendChild(document.createElement('h3'));
+  var $p = $divColHalf2.appendChild(document.createElement('p'));
+
+  $imgTag.setAttribute('src', entry.imageUrl);
+  $h3.textContent = entry.title;
+  $p.textContent = entry.notes;
+
+  return ($ul);
+}
